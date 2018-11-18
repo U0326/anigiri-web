983e27c7c2a944ac4872288d46f2bf07b7079187
Yuu Fujimura <koukokuiranaiyo@gmail.com>
2018-11-18 15:49:34 +0900

    vue-routerに対応した。

---
+++ src/main/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div>
+            <nav>アニメリストをここに表示する。</nav>
+        </div>
+        <div>
+            <router-view />
+        </div>
+    </div>
+</template>
+
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
-import App from './components/App.vue';
+import App from './App.vue';
 import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/ja'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import router from './router.js'
+
 window.onload = function() {
     Vue.use(ElementUI);
-    new Vue(App).$mount('#app');
+    new Vue({
+        el: '#app',
+        router,
+        render: h => h(App)
+    })
 }
