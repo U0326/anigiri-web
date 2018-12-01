1843544360f059e5fcfff1001cb5f428b24fbe03
Yuu Fujimura <koukokuiranaiyo@gmail.com>
2018-12-01 19:19:06 +0900

    アニメリスト内の各クールの明細を遅延初期化する処理を追加した。

--- src/main/js/CourNavigation.vue
+++ src/main/js/CourNavigation.vue
@@ -1,62 +1,54 @@
 <template>
-    <el-tree node-key="id" :data="nodes" :default-expanded-keys="[selectedKey]" :highlight-current=true
-            @node-click="handleNodeClick" ref="tree" />
+    <el-tree :props="props" node-key="id" :default-expanded-keys="expandedKeys" :highlight-current=true
+            @node-click="handleNodeClick" ref="tree" :load="loadNodes" lazy />
 </template>
 
 <script>
     // TODO 以下ダミーデータの為、削除する必要あり。
-    import dummy_data from '../../test/resources/cours.json'
-
+    import graphRowData from '../../test/resources/2018_04_cour.json'
     export default {
         data: function() {
             return {
-                nodes: null,
-                selectedKey: null,
-                defaultProps: {
+                props: {
+                    id: 'id',
+                    label: 'label',
                     children: 'children',
-                    label: 'label'
-                }
+                    isLeaf: 'isLeaf',
+                },
+                treeNodes: this.$store.state.sortedCours,
+                expandedKeys: null,
             }
-        },
-        created: function() {
-            // TODO 以下ダミーデータの為、削除する必要あり。
-            this.nodes = this.convertToTree(dummy_data);
-
         },
         mounted: function() {
-            this.selectedKey = this.$route.params.year + '-' + this.$route.params.cour;
-            this.$refs.tree.setCurrentKey(this.selectedKey);
-        },
-        watch: {
-            selectedKey: function() {
-                this.$refs.tree.setCurrentKey(this.selectedKey);
-            }
+            this.expandedKeys = [this.$route.params.year,
+                    this.$route.params.year + '-' + this.$route.params.cour];
         },
         methods: {
-            convertToTree: function(json) {
-                let sortedTerm = json.term.sort(function(x, y) {
-                    if (x.year < y.year) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
-                });
-                let nodes = [];
-                // TODO 各クールのアニメタイトルの表示は、クリックしたタイミングで取得を行う。
-                for (let i = 0; i < sortedTerm.length; i++) {
-                    nodes.push({
-                        id: sortedTerm[i].year,
-                        label: sortedTerm[i].year + '年',
-                        children: sortedTerm[i].cours.sort().map(function(element) {
-                            return {
-                                id: sortedTerm[i].year +'-' + element,
-                                label: '第' + element + 'クール(' + (1 + 3 * (element - 1)) + '月〜' +
-                                        (3 + 3 * (element - 1)) + '月)'
-                            }
-                        })
-                    });
+            loadNodes: function(node, resolve) {
+                if (node.level === 0) {
+                    return resolve(this.treeNodes.map(function(element) {
+                        return {id: element.year, label: element.year + '年'};
+                    }));
                 }
-                return nodes;
+                if (node.level === 1) {
+                    return resolve(this.treeNodes.find(function(element) {
+                        return element.year === node.data.id;
+                    }).cours.map(function(element) {
+                        return {id: node.data.id + '-' + element,
+                                label: '第' + element + 'クール(' + (1 + 3 * (element - 1)) + '月〜' +
+                                (3 + 3 * (element - 1)) + '月)'};
+                    }));
+                }
+                if (node.level === 2) {
+                    // 画面表示時にハイライトを行う為の記載である。
+                    this.$refs.tree.setCurrentKey(node.data.id);
+                    // TODO 選択したクールのアニメ一覧を取得する処理が必要である。
+                    setTimeout(() => {
+                        this.$store.commit('updateGraphRowData', graphRowData);
+                        return resolve([{id: 1, label: 1, isLeaf: true}, {id: 2, label: 2, isLeaf: true}]);
+                    }, 3000);
+                }
+
             },
             handleNodeClick: function(data) {
                 // TODO 個々のアニメ毎の詳細グラフの表示画面へ遷移する。
